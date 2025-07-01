9fd9a462bad78500aa3e92ce967accd130e22cda
Nuran <nuranbayramli69@gmail.com>
2025-07-01 03:07:17 +0200

    Deleted the unwanted files from repo

--- exceptions.py
+++
@@ -1,8 +0,0 @@
-x = 0
-while x == 0:
-    try:
-        num_1 = int(input())
-        a = 5 // num_1
-        print("here is our number, daddy: " + str(a))
-    except ZeroDivisionError:
-        print("Enter smth diff from 0 u dumass")
